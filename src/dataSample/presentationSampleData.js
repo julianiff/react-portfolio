0c03c4c8831f5fa683a9882dce427de8097f3fc1
julianiff <jule_iff@mac.com>
2019-06-26 15:49:51 +0200

    Updated data sample to reflect that jsx is possible in the data file.

--- src/dataSample/presentationSampleData.js
+++ src/dataSample/presentationSampleData.js
@@ -11,7 +11,7 @@ export const presentationSampleFlow = {
     next: 'next', // string referencee to the next component
     content: {
       template: "Default", // Uses the default template
-      title: "Add your Title here", // Here Add a title Optional
+      title: <>Add your Title here</>, // Here Add a title Optional
       subtitle: "Add your subtitle here", // Here ad a subtitle Optional
       img: {
         src: reactSVG, // Here add the source Optional
